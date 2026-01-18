29958ae22895a79fd720f3e845d2b9cfda9a9626
Pablo <padoro89@gmail.com>
2026-01-18 22:01:56 +0100

    new config to screen's size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xxs: "360px",
+        // => @media (min-width: 360px) { ... }
+        xs: "390px",
+        // => @media (min-width: 360px) { ... }
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+
+        "3xl": "1900px",
+        // => @media (min-width: 1900px) { ... }
+      },
+    },
   },
   plugins: [],
 };
